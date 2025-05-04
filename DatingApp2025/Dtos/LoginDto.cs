b6a120254f597f4b199e5283abc97ba6f3321a26
Umut Albayrak <23390008061@yyu.edu.tr>
2025-05-05 01:33:39 +0300

    Add files via upload

---
+++ DatingApp2025/Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingApp2025.Dtos
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DatingApp2025/Dtos/UserPatchDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatingApp2025.Dtos
+{
+    public class UserPatchDto
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Bio { get; set; }
+        public string? Gender { get; set; }
+        public string? SexualOrientation { get; set; }
+        public string? Location { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public List<string>? Hobbies { get; set; }
+        public string? Occupation { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
